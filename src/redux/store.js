16ef308d009bbbb10864e41d00b231bc52317025
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-05-03 14:40:54 +0000

    final commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
 import { counterReducer } from "./reducer/counterRedcuer";
+import { configureStore } from '@reduxjs/toolkit';
 import {createStore} from 'redux'
+export const store = configureStore({
+    reducer: {
+      counter: counterReducer
+    }
+  });
 
 
-export const store= createStore(counterReducer)
+//export const store= createStore(counterReducer)
